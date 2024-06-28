9cfca74dc797a664847ebb409fd2e9cdc198f0bd
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-06-28 17:30:09 -0500

    update tsconfig

--- src/main.ts
+++ src/main.ts
@@ -20,158 +20,156 @@ export interface IHDPrice {
 
 const NAME_SCRAPE_ERROR = "NAME_SCRAPE_ERROR";
 
-(async () => {
-  await Actor.main(async () => {
-    // const startUrls = [
-    //   "https://www.homedepot.com/p/Husky-56-in-W-x-22-in-D-Heavy-Duty-23-Drawer-Combination-Rolling-Tool-Chest-and-Top-Tool-Cabinet-Set-in-Matte-Black-HOTC5623BB2S/303412321",
-    // ];
-    const input = (await KeyValueStore.getInput()) as Dictionary;
+await Actor.main(async () => {
+  // const startUrls = [
+  //   "https://www.homedepot.com/p/Husky-56-in-W-x-22-in-D-Heavy-Duty-23-Drawer-Combination-Rolling-Tool-Chest-and-Top-Tool-Cabinet-Set-in-Matte-Black-HOTC5623BB2S/303412321",
+  // ];
+  const input = (await KeyValueStore.getInput()) as Dictionary;
 
-    const { startUrls } = input;
+  const { startUrls } = input;
 
-    // Prepare puppeteer plugins
-    puppeteerExtra.use(stealthPlugin());
-    puppeteerExtra.use(replPlugin());
-    puppeteerExtra.use(
-      addblockerPlugin({
-        // Optionally enable Cooperative Mode for several request interceptors
-        interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
-      }),
-    );
-    // Create a PuppeteerCrawler
-    const crawler = new PuppeteerCrawler({
-      // proxyConfiguration,
-      // requestHandler: router,
-      launchContext: {
-        launcher: puppeteerExtra,
-        launchOptions: {
-          headless: true,
-          defaultViewport: { width: 1280, height: 720 },
-        },
+  // Prepare puppeteer plugins
+  puppeteerExtra.use(stealthPlugin());
+  puppeteerExtra.use(replPlugin());
+  puppeteerExtra.use(
+    addblockerPlugin({
+      // Optionally enable Cooperative Mode for several request interceptors
+      interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+    }),
+  );
+  // Create a PuppeteerCrawler
+  const crawler = new PuppeteerCrawler({
+    // proxyConfiguration,
+    // requestHandler: router,
+    launchContext: {
+      launcher: puppeteerExtra,
+      launchOptions: {
+        headless: true,
+        defaultViewport: { width: 1280, height: 720 },
       },
-      maxRequestRetries: Number(process.env.MAX_REQUEST_RETRIES) ?? 0,
-      maxRequestsPerCrawl: Number(process.env.MAX_REQUESTS_PER_CRAWL) ?? 1,
-      navigationTimeoutSecs: Number(process.env.NAVIGATION_TIMEOUT_SECS) ?? 60,
-      requestHandlerTimeoutSecs:
-        Number(process.env.REQUEST_HANDLER_TIMEOUT_SECS) ?? 60,
-      // Activates the Session pool (default is true).
-      useSessionPool:
-        process.env.USE_SESSION_POOL?.toLowerCase() === "false" ? false : true,
-      // Overrides default Session pool configuration
-      sessionPoolOptions: {
-        maxPoolSize: Number(process.env.MAX_POOL_SIZE) ?? 10,
-      },
-      // Set to true if you want the crawler to save cookies per session,
-      // and set the cookies to page before navigation automatically (default is true).
-      persistCookiesPerSession:
-        process.env.PERSIST_COOKIES_PER_SESSION?.toLowerCase() === "false"
-          ? false
-          : true,
-      autoscaledPoolOptions: {
-        maxConcurrency: Number(process.env.MAX_CONCURRENCY) ?? 1,
-      },
+    },
+    maxRequestRetries: Number(process.env.MAX_REQUEST_RETRIES) ?? 0,
+    maxRequestsPerCrawl: Number(process.env.MAX_REQUESTS_PER_CRAWL) ?? 1,
+    navigationTimeoutSecs: Number(process.env.NAVIGATION_TIMEOUT_SECS) ?? 60,
+    requestHandlerTimeoutSecs:
+      Number(process.env.REQUEST_HANDLER_TIMEOUT_SECS) ?? 60,
+    // Activates the Session pool (default is true).
+    useSessionPool:
+      process.env.USE_SESSION_POOL?.toLowerCase() === "false" ? false : true,
+    // Overrides default Session pool configuration
+    sessionPoolOptions: {
+      maxPoolSize: Number(process.env.MAX_POOL_SIZE) ?? 10,
+    },
+    // Set to true if you want the crawler to save cookies per session,
+    // and set the cookies to page before navigation automatically (default is true).
+    persistCookiesPerSession:
+      process.env.PERSIST_COOKIES_PER_SESSION?.toLowerCase() === "false"
+        ? false
+        : true,
+    autoscaledPoolOptions: {
+      maxConcurrency: Number(process.env.MAX_CONCURRENCY) ?? 1,
+    },
 
-      async requestHandler(ctx) {
-        const { request, page, log, session, pushData } = ctx;
-        page.setDefaultTimeout(
-          Number(process.env.PAGE_DEFAULT_TIMEOUT_MILLIS) ?? 60000,
+    async requestHandler(ctx) {
+      const { request, page, log, session, pushData } = ctx;
+      page.setDefaultTimeout(
+        Number(process.env.PAGE_DEFAULT_TIMEOUT_MILLIS) ?? 60000,
+      );
+
+      const title = await page.title();
+      const url = request.loadedUrl;
+
+      log.info(`crawling ${url}`, { title, url });
+      if (session) {
+        if (title === "Blocked") {
+          session.retire();
+        } else if (
+          title === "Not sure if blocked, might also be a connection error"
+        ) {
+          session.markBad();
+        }
+        // this step is done automatically in PuppeteerCrawler.
+        // else { session.markGood() }
+      }
+
+      const priceContainer = "#standard-price > div > div";
+
+      // This is the div that houses the multiple 'span' components that make
+      // up the price
+      const priceComponent = await page.waitForSelector(priceContainer);
+      if (priceComponent) {
+        const hdPrice: IHDPrice = await priceComponent.$$eval(
+          "span",
+          async (els) => {
+            const data: string[] = [];
+            for (const span of els) {
+              const price = span.textContent;
+              if (price) {
+                data.push(price);
+              }
+            }
+            // Join together the multiple span components to form the price as
+            // a single string
+            return { price: data.join("") };
+          },
         );
 
-        const title = await page.title();
-        const url = request.loadedUrl;
+        if (hdPrice) {
+          log.info("scraped price", { price: hdPrice.price });
 
-        log.info(`crawling ${url}`, { title, url });
-        if (session) {
-          if (title === "Blocked") {
-            session.retire();
-          } else if (
-            title === "Not sure if blocked, might also be a connection error"
-          ) {
-            session.markBad();
-          }
-          // this step is done automatically in PuppeteerCrawler.
-          // else { session.markGood() }
-        }
+          // Scrape product name
+          let productName;
+          try {
+            const titleDiv = await page.waitForSelector(
+              ".product-details__badge-title--wrapper",
+              { timeout: 60000 },
+            );
 
-        const priceContainer = "#standard-price > div > div";
-
-        // This is the div that houses the multiple 'span' components that make
-        // up the price
-        const priceComponent = await page.waitForSelector(priceContainer);
-        if (priceComponent) {
-          const hdPrice: IHDPrice = await priceComponent.$$eval(
-            "span",
-            async (els) => {
-              const data: string[] = [];
-              for (const span of els) {
-                const price = span.textContent;
-                if (price) {
-                  data.push(price);
-                }
-              }
-              // Join together the multiple span components to form the price as
-              // a single string
-              return { price: data.join("") };
-            },
-          );
-
-          if (hdPrice) {
-            log.info("scraped price", { price: hdPrice.price });
-
-            // Scrape product name
-            let productName;
-            try {
-              const titleDiv = await page.waitForSelector(
-                ".product-details__badge-title--wrapper",
-                { timeout: 60000 },
+            if (titleDiv) {
+              productName = await titleDiv.$eval(
+                "span > h1",
+                (title) => title.textContent,
               );
-
-              if (titleDiv) {
-                productName = await titleDiv.$eval(
-                  "span > h1",
-                  (title) => title.textContent,
-                );
-              }
-            } catch (e) {
-              const err = e as Error;
-              log.error("failed scraping product name", {
-                error: { name: err.name, message: err.message },
-              });
             }
-
-            hdPrice.name = productName ?? NAME_SCRAPE_ERROR;
-            hdPrice.url = startUrls[0].url;
-
-            log.info("scraped data", hdPrice);
-          } else {
-            log.error("data not found", priceComponent);
+          } catch (e) {
+            const err = e as Error;
+            log.error("failed scraping product name", {
+              error: { name: err.name, message: err.message },
+            });
           }
 
-          // Take a screenshot for reference
-          const screenshotEncoding = "base64";
-          const type = "png";
+          hdPrice.name = productName ?? NAME_SCRAPE_ERROR;
+          hdPrice.url = startUrls[0].url;
 
-          const screenshot = await page.screenshot({
-            encoding: screenshotEncoding,
-            type,
-            optimizeForSpeed: true,
-            path: "../notes/screen.txt",
-          });
-
-          hdPrice.screenshot = {
-            encoding: screenshotEncoding,
-            type,
-            img: screenshot,
-          };
-
-          await pushData(hdPrice);
+          log.info("scraped data", hdPrice);
         } else {
-          log.error("selector not found", { selector: priceContainer, url });
+          log.error("data not found", priceComponent);
         }
-      },
-    });
 
-    // Run the crawler with the start URLs and wait for it to finish.
-    await crawler.run(startUrls);
+        // Take a screenshot for reference
+        const screenshotEncoding = "base64";
+        const type = "png";
+
+        const screenshot = await page.screenshot({
+          encoding: screenshotEncoding,
+          type,
+          optimizeForSpeed: true,
+          path: "../notes/screen.txt",
+        });
+
+        hdPrice.screenshot = {
+          encoding: screenshotEncoding,
+          type,
+          img: screenshot,
+        };
+
+        await pushData(hdPrice);
+      } else {
+        log.error("selector not found", { selector: priceContainer, url });
+      }
+    },
   });
-})();
+
+  // Run the crawler with the start URLs and wait for it to finish.
+  await crawler.run(startUrls);
+});
